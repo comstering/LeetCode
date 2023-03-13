66d39e5ac17a67baf08ae6b9fd5ae6b0751ecfb7
Hansu Choi <53170968+comstering@users.noreply.github.com>
2023-03-13 09:27:51 +0900

    Time: 164 ms (81.63%), Space: 35.4 MB (70.75%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.kt
@@ -0,0 +1,21 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun isSymmetric(root: TreeNode?): Boolean {
+        return root == null || isMirror(root.left, root.right)
+    }
+
+    fun isMirror(left: TreeNode?, right: TreeNode?): Boolean {
+        if (left == null && right == null) return true
+        if (left == null || right == null) return false
+        return left.`val` == right.`val` && isMirror(left.left, right.right) && isMirror(left.right, right.left)
+    }
+}
